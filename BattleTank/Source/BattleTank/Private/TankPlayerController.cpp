f3851cd2b579ea4b0b328ec25a2f4bcf0b5f2994
Chiel <chielakabyte@gmail.com>
2017-08-23 20:21:41 +0200

    BT31 Create Default Sub Objects in C++

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -35,6 +35,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; // Out parameter
 	if (GetSightRayHitLocation(HitLocation)) // Has ""side-effect", is going to line trace
 	{
+		// GetControlledTank()->AimAt(HitLocation);
 		GetControlledTank()->AimAt(HitLocation);
 	}
 }
